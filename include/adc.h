4a42760b34ee373a914f8e806a3fbe6aecb5a38a
Oscar <61410630+ozzwozz@users.noreply.github.com>
2024-05-29 13:51:19 +0100

    Add files via upload

--- include/adc.h
+++ include/adc.h
@@ -7,22 +7,20 @@
 /// @brief ADC Driver for reading from analogue to digital pins
 class ADC
 {
-private:
+    public:
+        /// @brief Construct a new ADC object
+        ADC();
+        /// @brief Destroy the ADC object
+        ~ADC();
 
-public:
-    /// @brief Construct a new ADC object
-    ADC();
-    /// @brief Destroy the ADC object
-    ~ADC();
+        /// @param supply_13V_pin 13V supply pin
+        const uint supply_13V_pin = 20;
+        /// @param PA_PGOOD_13V_pin 13V PA PGOOD pin
+        const uint PA_PGOOD_13V_pin = 1;
 
-    /// @param supply_13V_pin 13V supply pin
-    const uint supply_13V_pin = 20;
-    /// @param PA_PGOOD_13V_pin 13V PA PGOOD pin
-    const uint PA_PGOOD_13V_pin = 1;
+        /// @brief enable the 13V supply line
+        void enable_13V();
 
-    /// @brief enable the 13V supply line
-    void enable_13V();
-
-    /// @brief disable the 13V supply line
-    void disable_13V();
+        /// @brief disable the 13V supply line
+        void disable_13V();
 };
